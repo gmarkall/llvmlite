5383714d21a3ffb95aa73441febf76375eb62785
Graham Markall <gmarkall@nvidia.com>
2022-12-07 12:20:41 +0000

    Remove some old junk

--- ffi/orcjit.cpp
+++ ffi/orcjit.cpp
@@ -5,31 +5,6 @@
 #include "llvm-c/Orc.h"
 #include "llvm/ExecutionEngine/Orc/LLJIT.h"
 
-namespace llvm {
-namespace orc {
-
-inline LLJIT *unwrap(LLVMOrcLLJITRef P) {
-    return reinterpret_cast<LLJIT *>(P);
-}
-
-inline LLVMOrcLLJITRef wrap(const LLJIT *P) {
-    return reinterpret_cast<LLVMOrcLLJITRef>(
-        const_cast<LLJIT *>(P));
-}
-
-inline ThreadSafeModule *unwrap(LLVMOrcThreadSafeModuleRef P) {
-    return reinterpret_cast<ThreadSafeModule *>(P);
-}
-
-inline LLVMOrcThreadSafeModuleRef wrap(const ThreadSafeModule *P) {
-    return reinterpret_cast<LLVMOrcThreadSafeModuleRef>(
-        const_cast<ThreadSafeModule *>(P));
-}
-
-
-} // namespace orc
-} // namespace llvm
-
 using namespace llvm;
 
 extern "C" {
@@ -37,7 +12,7 @@ extern "C" {
 API_EXPORT(LLVMOrcLLJITRef)
 LLVMPY_CreateLLJITCompiler(const char **OutError) {
     auto builder = LLVMOrcCreateLLJITBuilder();
-    LLVMOrcLLJITRef JIT;// = orc::LLJITBuilder().create();
+    LLVMOrcLLJITRef JIT;
     auto error = LLVMOrcCreateLLJIT(&JIT, builder);
 
     if (error) {
@@ -63,18 +38,6 @@ LLVMPY_LLJITLookup(LLVMOrcLLJITRef JIT, const char *name)
     LLVMOrcExecutorAddress ea;
     LLVMOrcLLJITLookup(JIT, &ea, name);
     return ea;
-}
-
-API_EXPORT(LLVMOrcThreadSafeModuleRef)
-LLVMPY_CreateThreadSafeModule(LLVMModuleRef module/*, LLVMContextRef context*/)
-{
-  auto llvm_ts_ctx = LLVMOrcCreateNewThreadSafeContext();
-  auto tsm = LLVMOrcCreateNewThreadSafeModule(module, llvm_ts_ctx);
-  return tsm;
-  //return wrap(new orc::ThreadSafeModule(
-  //      std::move(
-  //        std::unique_ptr<Module>(unwrap(module))),
-  //        std::unique_ptr<LLVMContext>(std::move(unwrap(context)))));
 }
 
 } // extern "C"
